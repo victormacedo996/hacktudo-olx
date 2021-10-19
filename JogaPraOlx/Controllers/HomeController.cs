242bfe9951a4148a7a30c8fa46c4fc327b53c2b2
diogofeital <diiogofeital@gmail.com>
2021-10-19 00:33:27 -0300

    Ajustes HTML layout index

--- JogaPraOlx/Controllers/HomeController.cs
+++ JogaPraOlx/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using JogaPraOlx.Models;
 
 namespace JogaPraOlx.Controllers
 {
@@ -23,15 +22,5 @@ namespace JogaPraOlx.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
